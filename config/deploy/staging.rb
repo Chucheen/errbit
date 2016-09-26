db39a4c9ef7d7c79b210c5ef60d6dadd229a117d
JGutierrezC <jegut87@gmail.com>
2016-09-25 23:24:06 -0500

    Added files for staging2 and staging3 to avoid modify staging file multiple times

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server 'staging.homie.mx', user: 'deploy', roles: %w{app db web}
-
+set :rails_env, :staging
 
 # role-based syntax
 # ==================
